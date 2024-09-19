5bc593e483e3b5104f09b519e4f41225621fa0e6
Ell <71246161+Anonyko@users.noreply.github.com>
2024-09-19 16:51:06 +0700

    Update updatelagi.lua

--- updatelagi.lua
+++ updatelagi.lua
@@ -2,6 +2,31 @@ repeat
     task.wait()
 until game:IsLoaded()
 if hwid == "16003a41d764ac40af135c8682e2c1e1048718aeeb975093dab59604b1d81b2b" then
+    repeat task.wait() until game:IsLoaded()
+local Players   = game:GetService("Players")
+local Client    = Players.LocalPlayer
+local PlayerGui = Client.PlayerGui
+local kontolodn = PlayerGui:FindFirstChild("Hub")
+if kontolodn then
+
+local a = True
+
+while a do
+local argscreatps = {
+    [1] = "Create"
+}
+game:GetService("ReplicatedStorage").NetworkContainer.RemoteEvents.PrivateServer:FireServer(unpack(argscreatps))
+shared.PsCode = PlayerGui.Hub.Container.Window.PrivateServer.ServerLabel.Text
+local argsfirserver = {
+    [1] = "Join",
+    [2] = shared.PsCode,
+    [3] = "JawaTengah"
+}
+game:GetService("ReplicatedStorage").NetworkContainer.RemoteEvents.PrivateServer:FireServer(unpack(argsfirserver))
+wait(5)
+
+end
+else
     local Players = game:GetService("Players")
 local Client = Players.LocalPlayer
 local lives = workspace:WaitForChild("Lives")
@@ -585,6 +610,7 @@ local function gacorSay()
 end
 
 gacorSay()
+end
 else
     print("manahwidluanjing")
 end
